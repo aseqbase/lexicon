78d5d999799b0f41bf4c9a3fd55c74bdfa30c59d
Mohammad Fathi <mimfapost@gmail.com>
2025-07-15 13:20:30 +0330

    v3.2.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'aseqbase/lexicon',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a12f8f7b2e4bc6452f6426925acc9ef9fbe158bd',
+        'type' => 'directory',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'aseqbase/lexicon' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a12f8f7b2e4bc6452f6426925acc9ef9fbe158bd',
+            'type' => 'directory',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
